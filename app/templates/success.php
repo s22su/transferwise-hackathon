d44e381ad7ca318b6afea3a435c0377ba00c39ab
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2015-11-21 18:57:41 +0200

    added success page

--- app/templates/success.php
+++ app/templates/success.php
@@ -12,7 +12,41 @@
         </div>
     </div>
 
-    <div class="row">
-        adadsdas
-    </div>
+	<div class="row">
+		<div class="col-lg-12">
+			Payment info <small>001</small>
+		</div>
+	</div>
+	<div class="row">
+		<table class="table table-striped">
+			<tr>
+				<td><b>Item Bought:</b></td>
+				<td>Assorted Flowers</td>
+			</tr>
+			<tr>
+				<td><b>Name:</b></td>
+				<td>John Doe</td>
+			</tr>
+			<tr>
+				<td><b>Delivery address:</b></td>
+				<td>
+					<address>
+					  <strong>Windsor</strong><br>
+					  United Kingdom<br>
+					</address>
+				</td>
+			</tr>
+			<tr>
+				<td><b>Phone:</b></td>
+				<td>12345678</td>
+			</tr>
+			<tr>
+				<td><b>Price:</b></td>
+				<td>$ 20.00</td>
+			</tr>
+		</table>
+	</div>
+	
+	<a href="." class="btn btn-primary pull-right">Back to Home Page</a>
+		
 </div>
