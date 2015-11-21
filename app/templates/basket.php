5a9ccb6c410df3e2b765362a6ef1175292160abb
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2015-11-21 18:05:16 +0200

    basket page done

--- app/templates/basket.php
+++ app/templates/basket.php
@@ -16,7 +16,7 @@
 	<div class="row">
 
 		<div class="col-md-3">
-			<p class="lead">Domino's (Windsor, UK)</p>
+			<p>Windsor, UK Stores</p>
 			<div class="list-group">
 				<a href="basket" class="list-group-item">All Items</a>
 				<a href="pizza" class="list-group-item">Pizzas</a>
@@ -24,6 +24,14 @@
 				<a href="flower" class="list-group-item">Flowers</a>
 				<a href="other" class="list-group-item">Others</a>
 			</div>
+			<hr/>
+			<p>Send gift to:</p>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="text" class="form-control " id="send">
+				</div>
+			</div>
+			<a href="basket" class="btn btn-primary pull-right">Search</a>
 		</div>
 
 		<div class="col-md-9">
@@ -42,10 +50,10 @@
 								<img class="slide-image" src="img/flower/5.jpg" alt="">
 							</div>
 							<div class="item">
-								<img class="slide-image" src="img/pizza/12.jpg" alt="">
+								<img class="slide-image" src="img/toy/1.jpg" alt="">
 							</div>
 							<div class="item">
-								<img class="slide-image" src="img/pizza/13.jpg" alt="">
+								<img class="slide-image" src="img/toy/2.jpg" alt="">
 							</div>
 						</div>
 						<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
@@ -65,9 +73,10 @@
 						<img src="img/flower/1.jpg" alt="">
 						<div class="caption">
 							<h4 class="pull-right">$12.99</h4>
-							<h4><a href="#">Pepperoni</a>
+							<h4><a href="#">Assorted Flowers</a>
 							</h4>
 							<p>See more snippets like this online store item at</p>
+							<a href="pay" class="btn btn-success btn-large">Checkout</a>
 						</div>
 						<div class="ratings">
 							<p class="pull-right">15 reviews</p>
@@ -87,9 +96,10 @@
 						<img src="img/pizza/2.jpg" alt="">
 						<div class="caption">
 							<h4 class="pull-right">$15.99</h4>
-							<h4><a href="#">Hawaiian</a>
+							<h4><a href="#">Vegetarian</a>
 							</h4>
 							<p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="pay" class="btn btn-success btn-large">Checkout</a>
 						</div>
 						<div class="ratings">
 							<p class="pull-right">12 reviews</p>
@@ -108,10 +118,11 @@
 					<div class="thumbnail">
 						<img src="img/flower/3.jpg" alt="">
 						<div class="caption">
-							<h4 class="pull-right">$10.99</h4>
-							<h4><a href="#">Four Cheeses</a>
+							<h4 class="pull-right">$20.99</h4>
+							<h4><a href="#">Pure White Roses</a>
 							</h4>
 							<p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="pay" class="btn btn-success btn-large">Checkout</a>
 						</div>
 						<div class="ratings">
 							<p class="pull-right">31 reviews</p>
@@ -130,12 +141,13 @@
 			<div class="row">
 				<div class="col-sm-4 col-lg-4 col-md-4">
 					<div class="thumbnail">
-						<img src="img/pizza/4.jpg" alt="">
+						<img src="img/toy/4.jpg" alt="">
 						<div class="caption">
-							<h4 class="pull-right">$13.99</h4>
-							<h4><a href="#">Barbecue</a>
+							<h4 class="pull-right">$45.99</h4>
+							<h4><a href="#">Bicycle</a>
 							</h4>
 							<p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="pay" class="btn btn-success btn-large">Checkout</a>
 						</div>
 						<div class="ratings">
 							<p class="pull-right">6 reviews</p>
@@ -152,12 +164,13 @@
 
 				<div class="col-sm-4 col-lg-4 col-md-4">
 					<div class="thumbnail">
-						<img src="img/pizza/5.jpg" alt="">
+						<img src="img/toy/3.jpg" alt="">
 						<div class="caption">
-							<h4 class="pull-right">$15.99</h4>
-							<h4><a href="#">Chicken Supreme</a>
+							<h4 class="pull-right">$24.99</h4>
+							<h4><a href="#">The Hunger Games</a>
 							</h4>
 							<p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="pay" class="btn btn-success btn-large">Checkout</a>
 						</div>
 						<div class="ratings">
 							<p class="pull-right">18 reviews</p>
@@ -177,9 +190,10 @@
 						<img src="img/flower/6.jpg" alt="">
 						<div class="caption">
 							<h4 class="pull-right">$14.99</h4>
-							<h4><a href="#">Margarita</a>
+							<h4><a href="#">Pink Roses Bouquet</a>
 							</h4>
 							<p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<a href="pay" class="btn btn-success btn-large">Checkout</a>
 						</div>
 						<div class="ratings">
 							<p class="pull-right">18 reviews</p>
