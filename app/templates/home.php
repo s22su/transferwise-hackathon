780aac7e6cdb568e8a5becbef3d1341e4b4c9f55
Sergei Beregov <sergeiberegov@Sergeis-MacBook-Pro.local>
2015-11-21 17:51:18 +0200

    Front images changes

--- app/templates/home.php
+++ app/templates/home.php
@@ -76,7 +76,7 @@
             <div class="row no-gutter">
                 <div class="col-lg-4 col-sm-6">
                     <a href="basket" class="portfolio-box">
-                        <img src="img/portfolio/1.jpg" class="img-responsive" alt="">
+                        <img src="img/portfolio/7.jpg" class="img-responsive" alt="">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
@@ -100,7 +100,7 @@
                 </div>
                 <div class="col-lg-4 col-sm-6">
                     <a href="basket" class="portfolio-box">
-                        <img src="img/portfolio/3.jpg" class="img-responsive" alt="">
+                        <img src="img/portfolio/1.jpg" class="img-responsive" alt="">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
