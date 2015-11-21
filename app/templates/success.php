04a972d23b69876819b744de87ad2310dc8a3e60
Sergei Beregov <sergeiberegov@Sergeis-MacBook-Pro.local>
2015-11-21 18:46:45 +0200

    Success page

---
+++ app/templates/success.php
@@ -0,0 +1,18 @@
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="container">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Payment successful</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        adadsdas
+    </div>
+</div>
